98978b75e1d56a82a32360e2c3b68a5b746bd7cf
keanutoofa <keanu.toofa@gmail.com>
2026-03-24 16:00:45 +0100

    Add a new root to run vite

--- homework/frontend-pixels-war/vite.config.js
+++ homework/frontend-pixels-war/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: './pixels-war.html',
+    },
+  },
   server: {
+    open: '/pixels-war.html',
     proxy: {
       // This matches any request starting with /api
       '/api': {
